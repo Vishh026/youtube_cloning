6a180a5fc39f60acf9cd17ac23e34e24dd14342d
unknown <vaishnavidhavan026@gmail.com>
2025-11-10 15:52:08 +0530

    Removing credentials

--- src/Constant.jsx
+++ src/Constant.jsx
@@ -1,6 +1,5 @@
-export const YOUTUBE_OWN_API = "AIzaSyAOn7hMvUhRCw0Q9yL-58AFZWDTof57kFY"
-// export const YOUTUBE_OWN_API  = "AIzaSyAG1m0DfqJJpu59S70YbhfMNEx-OfA8k6Y"
-
+const YOUTUBE_OWN_API = import.meta.env.VITE_YOUTUBE_OWN_API
+console.log("key",YOUTUBE_OWN_API)
 export const YOUTUBE_VIDEOAPI = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ YOUTUBE_OWN_API;
 
 export const YOUTUBE_SINGLE_VIDEO = (videoId)=>  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${YOUTUBE_OWN_API}`
